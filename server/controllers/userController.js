ccba1fc0836d2be3810c8a78b91e6e060fdffc48
Shiv Shakti <141945568+ShivShakti7445@users.noreply.github.com>
2025-08-29 14:09:19 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,6 +22,9 @@ export const userEnrolledCourses = async (req, res) => {
         const userId = req.auth.userId
         const userData = await User.findById(userId)
             .populate('enrolledCourses')
+        if (!userData) {
+            return res.json({ success: false, message: 'User Not Found' })
+        }
         res.json({ success: true, enrolledCourses: userData.enrolledCourses })
     } catch (error) {
         res.json({ success: false, message: error.message })
